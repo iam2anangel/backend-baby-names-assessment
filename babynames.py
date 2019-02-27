db5bba1a92eea8869bcdacd834c18d781397ac3d
Jennifer Browning <browningjen@yahoo.com>
2019-02-26 20:24:23 -0500

    created summary files

--- babynames.py
+++ babynames.py
@@ -45,11 +45,44 @@ def extract_names(filename):
     followed by the name-rank strings in alphabetical order.
     ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
     """
-    # +++your code here+++
-    return
+    result = []
+    with open(filename) as f:
+        text = f.read()
+
+    return result
+
+    # get year using regex
+    year_match = re.match(r'Popularity\sin\s(\d\d\d\d)', text)
+    # could use assert year_match(for example) to test anywhere in code only when using debugger
+    if not year_match:
+        print("Could not extract the year")
+        return None
+    year = year.match.group(1)
+    print("Found year: {}".format(year))
+    result.append(year)
+
+    # get the baby names and their ranks, as tuples
+    tuples = re.findall(r'<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', test)
+
+    # Each tuple looks like this now: ('984', 'Keven', 'Emilia')
+    # From data in the tuple, insert into result list
+    names_to_rank = {}  # can have duplicate values but not duplicate keys
+    for rank, boy, girl in tuples:
+        if boy not in names_to_rank:
+            # creating a new dictionary entry, names_to_rank is unsorted(because you cannot sort a dictionary)
+            names_to_rank[boy] = rank
+        if girl not in names_to_rank:
+            names_to_rank[girl] = rank
+
+    # sorting namestorank by key which is the actual name
+    sorted_names = sorted(names_to_rank.keys())
+    for name in sorted_names:  # iterating through keys of names
+        result.append(name + ' ' + names_to_rank[name])
+
+    return result
 
 
-def create_parser():
+def main():
     """Create a cmd line parser object with 2 argument definitions"""
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -57,11 +90,6 @@ def create_parser():
     # The nargs option instructs the parser to expect 1 or more filenames.
     # It will also expand wildcards just like the shell, e.g. 'baby*.html' will work.
     parser.add_argument('files', help='filename(s) to parse', nargs='+')
-    return parser
-
-
-def main():
-    parser = create_parser()
     args = parser.parse_args()
 
     if not args:
@@ -69,13 +97,19 @@ def main():
         sys.exit(1)
 
     file_list = args.files
-
-    # option flag
     create_summary = args.summaryfile
 
-    # +++your code here+++
-    # For each filename, get the names, then either print the text output
-    # or write it to a summary file
+    for file in file_list:
+        names = extract_names(file)
+        text = '\n'.join(names)
+
+        if create_summary:
+            txtFile = open(file + '.summary', 'w')
+            txtFile.write(text)
+            txtFile.close()
+            pass
+        else:
+            print text
 
 
 if __name__ == '__main__':
